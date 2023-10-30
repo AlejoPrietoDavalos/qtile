55bcf20687775eb4af4d7ad9fe55831b39321f40
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-10-29 21:27:28 -0300

    paths

--- settings/_paths.py
+++ settings/_paths.py
@@ -1,14 +1,31 @@
-""" Para guardar los paths principales del proyecto."""
+""" Para guardar los paths principales del proyecto.
+
+- IMPORTANTE: El proyecto debe ser clonado dentro de ~/.config.
+"""
 from pathlib import Path
 
-path_root = Path.home() / ".config" / "qtile"
+from typing import List
+
+path_home = Path.home()
+path_root = path_home / ".config" / "qtile"
 path_themes_folder = path_root / "themes"
 
+path_images = path_home / "images"
+path_screenshots = path_images / "screenshots"
+path_wallpapers = path_images / "wallpapers"
+
+
+# TODO: paths_commands: List[Path], estaría bueno para
+# tener un conjunto de comandos de distintos proyectos
+# y tenerlos accesibles desde acá, ver como se puede hacer
+# algo con el .bashrc o zsh
+
+
 # Crear los folders en caso de haberse borrado.
-for _p in [path_themes_folder]:
-    _p.mkdir(exist_ok=True)
+path_themes_folder.mkdir(exist_ok=True)
+path_images.mkdir(exist_ok=True)
+path_screenshots.mkdir(exist_ok=True)
+path_wallpapers.mkdir(exist_ok=True)
 
 
-def get_path_theme(theme_name: str) -> Path:
-    # Hacer validacion del path y toda la pelota.
-    return path_themes_folder / f"{theme_name}.json"
+
