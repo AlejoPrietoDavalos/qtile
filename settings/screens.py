58c20a4bc3969320f330b117f460e93c280c61ed
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-10-29 21:34:59 -0300

    clean

--- settings/screens.py
+++ settings/screens.py
@@ -4,15 +4,8 @@ from libqtile import bar
 from libqtile.config import Screen
 
 
-
-
-
-from pathlib import Path
-
 def get_screen(widgets, cfg: ConfigQTILE) -> Screen:
     return Screen(
         top = bar.Bar(widgets, **cfg.bar.model_dump()),
-        **cfg.screen.model_dump(),
-        # FIXME: Sacar el wallpaper para afuera, y hacerlo con feh.
-        wallpaper = str(Path.home() / "images" / "wallpapers" / f"0.png")
+        **cfg.screen.model_dump()
     )
